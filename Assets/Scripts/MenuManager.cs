276282ef65a23292d48d12f5df615eaf6e91427c
brianphchen <brian770130@gmail.com>
2022-09-22 11:51:19 -0700

    stable version with menu

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,10 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public class MenuManager : MonoBehaviour
 {
     public void onStartGame(string SceneName) {
-        Application.LoadLevel(SceneName);
+        SceneManager.LoadScene(SceneName);
+    }
+    
+    public void QuitGame() {
+        #if UNITY_EDITOR
+            EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
     }
 }
