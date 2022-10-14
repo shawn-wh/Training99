e0b4a3df788234f091b428cdca461ffec7145b34
Cowbon <alexalex850520@gmail.com>
2022-10-14 00:15:53 -0700

    Rename invisible to Invincible

--- Assets/Scripts/VersusMode/InvinciblePropController.cs
+++ Assets/Scripts/VersusMode/InvinciblePropController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class InvisiblePropController : PropPrototype
+public class InvinciblePropController : PropPrototype
 {
     void Start()
     {
