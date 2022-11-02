b54cc5f31826ee328d92c5638bc9e329e77f8d4b
Yuan-Peng (Patrick) Yu <yuanpeng@usc.edu>
2022-11-02 10:20:48 -0700

    Add In-Game Reminders for shrinking & pass-through-wall

---
+++ Assets/Scripts/LV_Reminder.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LV_Reminder : MonoBehaviour
+{
+    public GameObject reminderPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            reminderPrefab.SetActive(true);
+            StartCoroutine(TimeDelay());
+        }
+    }
+
+    IEnumerator TimeDelay()
+    {
+        yield return new WaitForSeconds(3);
+        reminderPrefab.SetActive(false);
+    }
+}
